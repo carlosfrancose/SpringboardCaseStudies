29799422cb08015c3a9afb4af04cae152f76dbdd
Carlos Franco <26030318+carlosfrancose@users.noreply.github.com>
2026-02-12 13:31:27 -0800

    refactor: Updated LocalSQLConnection.py with first query
    Modified several methods and also added Pandas for prettier printing.

--- Unit19-SQL_Country_Club/LocalSQLConnection.py
+++ Unit19-SQL_Country_Club/LocalSQLConnection.py
@@ -1,51 +1,66 @@
 import sqlite3
+import pandas as pd
 from sqlite3 import Error
+from dataclasses import dataclass
 
- 
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by the db_file
+@dataclass(frozen=True)
+class Query:
+    """
+    A single question and its corresponding SQL query.
+    """
+    q_id: int
+    question: str
+    sql: str
+
+
+def create_connection(db_file) -> sqlite3.Connection:
+    """
+    Prints SQLite version and creates connection to SQLite database.
+
     :param db_file: database file
-    :return: Connection object or None
+    :return: Connection object
+    :rtype: sqlite3.Connection
     """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
- 
-    return conn
+    sql_v = sqlite3.version_info
+    print(f"sqlite version: {sql_v[0]}.{sql_v[1]}.{sql_v[2]}")
+    return sqlite3.connect(db_file)
 
- 
-def select_all_tasks(conn):
+def read_and_print_query(conn: sqlite3.Connection, query: Query) -> None:
     """
-    Query all rows in the tasks table
-    :param conn: the Connection object
-    :return:
-    """
-    cur = conn.cursor()
+    Runs SQL query and prints out results.
     
-    query1 = """
-        SELECT *
-        FROM FACILITIES
-        """
-    cur.execute(query1)
- 
-    rows = cur.fetchall()
- 
-    for row in rows:
-        print(row)
+    :param query: Query object (for question id and string)
+    :type query: Query
+    """
+    result = pd.read_sql_query(query.sql, conn)
+    print(f"\nQ{query.q_id}: {query.question}\n{result}\n")
 
+QUERIES: list[Query] = [
+    Query(q_id=1,
+          question=(
+              "Some of the facilities charge a fee to members, but some do not.\n"
+              "Write a SQL query to produce a list of the names of the facilities "
+              "that do."
+          ),
+          sql="""
+              SELECT name
+              FROM Facilities
+              WHERE membercost > 0;
+              """
+        )
+]
 
 def main():
     database = "sqlite_db_pythonsqlite.db"
  
-    # create a database connection
-    conn = create_connection(database)
-    with conn: 
-        print("2. Query all tasks")
-        select_all_tasks(conn)
+    try:
+        # Create a database connection
+        conn = create_connection(database)
+        with conn:
+            for q in QUERIES:
+                read_and_print_query(conn, q)
+    except sqlite3.Error as e:
+        print(e)
  
  
 if __name__ == '__main__':
